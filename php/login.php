4e8b3ca2cf450c62770ee18b9354069b5be33053
nicolasque <37843574+nicolasque@users.noreply.github.com>
2024-03-21 19:06:46 +0100

    Update login.php with jQuery and login.js

--- php/login.php
+++ php/login.php
@@ -1,29 +1,5 @@
-<?php
-
-include 'navbar.php';
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Check if the username and password are provided
-    if (isset($_POST['username']) && isset($_POST['password'])) {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-
-        // TODO: Validate the username and password against your authentication system
-        // For example, you can check against a database or an API
-
-        // If the username and password are valid, redirect to the home page
-        if ($username === 'admin' && $password === 'password') {
-            header('Location: home.php');
-            exit;
-        } else {
-            $error = 'Invalid username or password';
-        }
-    } else {
-        $error = 'Please enter both username and password';
-    }
-}
-
-?>
+<script src="../js/jquery.js"></script>
+  <script src="js/login.js"></script>
 
 <!-- HTML code for the login form -->
 <form method="POST" action="">
